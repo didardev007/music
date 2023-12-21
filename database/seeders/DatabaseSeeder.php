ad323ee92fbd96a218d4a7cc49b7252206c67b2f
didardev007 <didarmeredow007007@gmail.com>
2023-12-22 03:26:05 +0500

    commit by Didar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GenreSeeder::class,
         ]);
-        \App\Models\User::factory(10)->create();
-        \App\Models\Artist::factory(10)->create();
-        \App\Models\Album::factory(10)->create();
-        \App\Models\Track::factory(10)->create();
+        \App\Models\User::factory(100)->create();
+        \App\Models\Artist::factory(100)->create();
+        \App\Models\Album::factory(100)->create();
+        \App\Models\Track::factory(100)->create();
     }
 }
