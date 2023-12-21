42093f023e9f2b3470e4e880ce4dae612bdd8cb8
didardev007 <didarmeredow007007@gmail.com>
2023-12-22 03:21:00 +0500

    commit by Didar

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
             ];
         }
 
-        return view('home.index')
+        return view('client.home.index')
             ->with([
                 'genreTracks' => $genreTracks,
             ]);
