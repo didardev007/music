1b9e6e762ec60173a40846a4eaf2d0f38bf6867d
didardev007 <didarmeredow007007@gmail.com>
2023-12-22 01:47:53 +0500

    seeders by didardev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            GenreSeeder::class,
+        ]);
+        \App\Models\User::factory(10)->create();
+        \App\Models\Album::factory(10)->create();
+        \App\Models\Artist::factory(10)->create();
+        \App\Models\Track::factory(10)->create();
     }
 }
--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $genres = [
+            'Speed Up',
+            'Slowed + Reverb',
+            'Indie Music',
+            'Jazz',
+            'Opera',
+            'Remix',
+            'Acoustic',
+            'Rock',
+            'Rep & Hip Hop',
+            'Pop',
+        ];
+
+        foreach ($genres as $genre) {
+            $el = new Genre();
+            $el->name = $genre;
+            $el->save();
+        }
     }
 }
