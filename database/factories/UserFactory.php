8bba2342565346d990de6268e1245a95433b5534
Ghost-developer-04 <selimmeredow8@gmail.com>
2023-12-22 02:32:48 +0500

    factory by ghost0701

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('superman'),
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
