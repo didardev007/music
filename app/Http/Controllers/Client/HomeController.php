3bfc20ff7c34f7827e605c05717fe56ea25d29a7
didardev007 <didarmeredow007007@gmail.com>
2023-12-22 03:03:17 +0500

    controllers by didardev fix

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
+use App\Models\Track;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $genres = Genre::withCount('track')
+            ->orderBy('Tracks_count' ,'desc')
+            ->take(7)
+            ->get();
+
+        $genreTracks = [];
+        foreach ($genres as $genre) {
+            $genreTracks[] = [
+                'genre' => $genre,
+                'Tracks' => Track::where('genre_id', $genre->id)
+                ->with('genre')
+                ->take(8)
+                ->get(),
+            ];
+        }
+
+        return view('home.index')
+            ->with([
+                'genreTracks' => $genreTracks,
+            ]);
+    }
 }
