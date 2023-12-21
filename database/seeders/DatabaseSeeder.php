42a43aa2ec2dd0dd13f5412bf3c4b200a27d719d
Ghost-developer-04 <selimmeredow8@gmail.com>
2023-12-22 02:34:12 +0500

    factory by ghost0701

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
             GenreSeeder::class,
         ]);
         \App\Models\User::factory(10)->create();
-        \App\Models\Album::factory(10)->create();
         \App\Models\Artist::factory(10)->create();
+        \App\Models\Album::factory(10)->create();
         \App\Models\Track::factory(10)->create();
     }
 }
