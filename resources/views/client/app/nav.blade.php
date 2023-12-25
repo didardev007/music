5cd657a8fd9140e68cf723a433ae464c979461c2
Ghost-developer-04 <selimmeredow8@gmail.com>
2023-12-26 00:06:17 +0500

    Commited by ghost0701

--- resources/views/client/app/nav.blade.php
+++ resources/views/client/app/nav.blade.php
@@ -1,14 +1,24 @@
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
             <span class="bi bi-chevron-double-down"></span>
         </button>
-        <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-        <div class="btn">
-            <img src="{{asset('img/92303_russia_icon.png')}}">
+        <div class="d-block">
+            <button class="btn bi bi-search" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"></button>
+            <div class="collapse" id="collapseExample">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search">
+                    <button class="btn btn-outline-success btn-sm bi bi-search" type="submit"></button>
+                </form>
+            </div>
+        </div>
+        <div class="d-flex">
+            <button class="btn btn-sm text-danger-emphasis">
+                Rus
+            </button>
+            <button class="btn btn-sm text-danger-emphasis">
+                Eng
+            </button>
         </div>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <a class="navbar-brand text-danger-emphasis" href="#">Music</a>
