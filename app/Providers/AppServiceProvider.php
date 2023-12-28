380a7bf7e419d2da29ce21bb6e060a06048f589e
didardev007 <didarmeredow007007@gmail.com>
2023-12-28 22:22:07 +0500

    commit by Didar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Genre;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\View;
 use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +26,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+        Model::preventLazyLoading(! app()->isProduction());
+        Paginator::useBootstrapFive();
+
+        Facades\View::composer('app.nav', function (View $view) {
+            $genres = Genre::withCount('tracks')
+                ->orderBy('name')
+                ->get();
+
+            $view->with([
+                'genres' => $genres,
+            ]);
+        });
     }
 }
