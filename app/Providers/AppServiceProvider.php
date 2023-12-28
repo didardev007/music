ea41220413243f51c4127175fa0f9befb6eccc65
didardev007 <didarmeredow007007@gmail.com>
2023-12-28 22:35:56 +0500

    commit by Didardev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,20 +2,13 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
+
 use App\Models\Genre;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades;
 use Illuminate\Pagination\Paginator;
-=======
 use App\Models\Album;
 use App\Models\Artist;
-use App\Models\Genre;
-use App\Models\Playlist;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades;
->>>>>>> lump/main
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\View;
 use Laravel\Sanctum\Sanctum;
@@ -36,14 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
-<<<<<<< HEAD
         Model::preventLazyLoading(! app()->isProduction());
-        Paginator::useBootstrapFive();
-
-        Facades\View::composer('app.nav', function (View $view) {
-            $genres = Genre::withCount('tracks')
-                ->orderBy('name')
-=======
         Paginator::useBootstrapFive();
 
         Facades\View::composer('app.nav', function (View $view) {
@@ -57,18 +43,16 @@ class AppServiceProvider extends ServiceProvider
                 ->get();
 
             $playlists = Playlist::orderBy('name')
->>>>>>> lump/main
+
                 ->get();
 
             $view->with([
                 'genres' => $genres,
-<<<<<<< HEAD
-=======
                 'artists' => $artists,
                 'albums' => $albums,
                 'playlists' => $playlists,
                 'user' => auth()->user(),
->>>>>>> lump/main
+
             ]);
         });
     }
