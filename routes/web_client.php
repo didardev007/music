576642a0ea4b33996d6c4385f0c99d2e07013278
didardev007 <didarmeredow007007@gmail.com>
2024-01-04 01:37:20 +0500

    commit by didardev

--- routes/web_client.php
+++ routes/web_client.php
@@ -1,11 +1,13 @@
 <?php
 
 
+use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Client\ArtistController;
 use App\Http\Controllers\Client\AlbumController;
 use App\Http\Controllers\Client\GenreController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\PlaylistController;
+use App\Http\Controllers\Client\RegisterController;
 use App\Http\Controllers\Client\TrackController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +18,19 @@ Route::controller(HomeController::class)
     ->group(function() {
         Route::get('', 'index')->name('home');
         Route::get('locale/{locale}', 'locale')->name('locale')->where('locale', '[a-z]+');
+    });
+
+Route::middleware('guest')
+    ->group(function () {
+        Route::get('register', [RegisterController::class])->name('register');
+        Route::post('register', [RegisterController::class]);
+        Route::get('login', [LoginController::class, 'create'])->name('login');
+        Route::post('login', [LoginController::class, 'store']);
+    });
+
+Route::middleware('auth')
+    ->group(function () {
+        Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
     });
 
 Route::resource('tracks', TrackController::class);
