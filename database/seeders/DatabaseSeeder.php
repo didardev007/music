7239f248de34bbbe052130543120d90c86ef3953
tirkeshdev <tirkesgeldigarayew@gmail.com>
2024-01-04 01:35:35 +0500

    commited by tirkeshdev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             GenreSeeder::class,
         ]);
         \App\Models\User::factory(10)->create();
