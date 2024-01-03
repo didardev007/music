c81d0ece989804aa8f6b7c8a0a39328f8b814b0d
Ghost-developer-04 <selimmeredow8@gmail.com>
2024-01-04 01:40:41 +0500

    Commited by ghost0701

--- routes/web_client.php
+++ routes/web_client.php
@@ -1,11 +1,13 @@
 <?php
 
 
+use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Client\ArtistController;
 use App\Http\Controllers\Client\AlbumController;
 use App\Http\Controllers\Client\GenreController;
 use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\PlaylistController;
+use App\Http\Controllers\Client\RegisterController;
 use App\Http\Controllers\Client\TrackController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +18,14 @@ Route::controller(HomeController::class)
     ->group(function() {
         Route::get('', 'index')->name('home');
         Route::get('locale/{locale}', 'locale')->name('locale')->where('locale', '[a-z]+');
+    });
+
+Route::middleware('guest')
+    ->group(function () {
+        Route::get('register', [RegisterController::class, 'create'])->name('register');
+        Route::post('register', [RegisterController::class, 'store']);
+        Route::get('login', [LoginController::class, 'create'])->name('login');
+        Route::post('login', [LoginController::class, 'store']);
     });
 
 Route::resource('tracks', TrackController::class);
