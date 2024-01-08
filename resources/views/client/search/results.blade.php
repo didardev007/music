e3f892e3f667b88b4046f5a7758605d460465e19
didardev007 <didarmeredow007007@gmail.com>
2024-01-09 02:19:20 +0500

    commit by Didar

--- resources/views/client/search/results.blade.php
+++ resources/views/client/search/results.blade.php
@@ -2,8 +2,8 @@
 
 @section('main')
     <div class="container-xl bg-light">
-        <div class="input-group px-5 py-3">
-            <form class="d-flex" action="{{ route('search') }}" method="get">
+        <div>
+            <form class="d-flex input-group px-5 py-3" action="{{ route('search') }}" method="get">
                 @csrf
                 <input type="text" class="form-control" name="query" value="{{ old('query', $query ?? '') }}" placeholder="@lang('app.search')">
                 <button class="btn btn-outline-secondary" type="submit">@lang('app.search')</button>
