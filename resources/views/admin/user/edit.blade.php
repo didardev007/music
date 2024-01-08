16776ac686d36cabe8846271ec4a72e194f22461
tirkeshdev <tirkesgeldigarayew@gmail.com>
2024-01-09 01:58:03 +0500

    commited by tirkeshdev

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -31,7 +31,7 @@
 
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
+            <input type="password" class="form-control" id="password" name="password" required>
         </div>
 
         <div class="mb-3">
