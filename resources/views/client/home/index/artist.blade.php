5c276db34d1bf46cb78b166444682e3822e960e3
tirkeshdev <tirkesgeldigarayew@gmail.com>
2024-01-09 01:12:54 +0500

    commited by tirkeshdev

--- resources/views/client/home/index/artist.blade.php
+++ resources/views/client/home/index/artist.blade.php
@@ -10,6 +10,14 @@
                         @include('client.app.artist')
                     </li>
                 @endforeach()
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3">
+                            <audio id="audioPlayer" controls>
+                                <source src="{{ asset('track/1668705065_ahmet-orazgulyyew-dam-dam-2022.mp3') }}" type="audio/mp3">
+                                Your browser does not support the audio tag.
+                            </audio>
+                        </div>
+                    </div>
             </ul>
         </div>
     </section>
