06bd0ebf78bb16627bd20b1d569462b9a2c530f2
Ghost-developer-04 <selimmeredow8@gmail.com>
2024-01-08 23:00:44 +0500

    Commited by ghost0701

--- resources/views/client/auth/register.blade.php
+++ resources/views/client/auth/register.blade.php
@@ -2,29 +2,55 @@
 @section('title') Music.com | Register @endsection
 @section('main')
     <div class="container-xl bg-light">
-        <form class="pt-4 px-5">
-            <!-- Name input -->
+        <form action="{{ route('register') }}" class="pt-4 px-5" method="post">
+        @csrf
+        <!-- Name input -->
             <div class="form-outline pb-4">
-                <input type="email" id="form2Example1" class="form-control"/>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+                       value="{{ old('name') }}" required autofocus>
                 <label class="form-label" for="form2Example1">@lang('app.name')</label>
+                @error('name')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
 
             <!-- Username input -->
             <div class="form-outline pb-4">
-                <input type="email" id="form2Example1" class="form-control"/>
-                <label class="form-label" for="form2Example1">@lang('app.username')</label>
+                <input type="text" class="form-control @error('username') is-invalid @enderror" id="username"
+                       name="username" value="{{ old('username') }}" required>
+                <label class="form-label" for="username">@lang('app.username')</label>
+                @error('username')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
 
             <!-- Email input -->
             <div class="form-outline mb-4">
-                <input type="email" id="form2Example1" class="form-control"/>
-                <label class="form-label" for="form2Example1">@lang('app.email')</label>
+                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                       name="email" value="{{ old('email') }}" required>
+                <label class="form-label" for="email">@lang('app.email')</label>
+                @error('email')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
 
             <!-- Password input -->
             <div class="form-outline pb-4">
-                <input type="password" id="form2Example2" class="form-control"/>
-                <label class="form-label" for="form2Example2">@lang('app.password')</label>
+                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
+                       name="password" value="{{ old('password') }}" required>
+                <label class="form-label" for="password">@lang('app.password')</label>
+                @error('password')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-outline pb-4">
+                <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror"
+                       id="password_confirmation" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
+                <label class="form-label" for="password_confirmation">@lang('app.passwordConfirmation')</label>
+                @error('password_confirmation')
+                <div class="alert alert-danger mt-2">{{ $message }}</div>
+                @enderror
             </div>
 
             <!-- 2 column grid layout for inline styling -->
@@ -40,7 +66,7 @@
 
             <!-- Submit button -->
             <div class="text-center pb-4">
-                <button type="button" class="btn btn-primary btn-block pb-4">@lang('app.register')</button>
+                <button type="submit" class="btn btn-primary btn-block pb-4">@lang('app.register')</button>
             </div>
 
             <!-- Register buttons -->
