888457d8216c2673d2230c26bd5b15f530f3a354
Ghost-developer-04 <selimmeredow8@gmail.com>
2024-01-09 02:45:29 +0500

    Commited by ghost0701

--- database/migrations/2023_12_21_201036_create_artists_table.php
+++ database/migrations/2023_12_21_201036_create_artists_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_ru')->nullable();
             $table->string('slug')->unique();
             $table->string('date_of_birth')->nullable();
             $table->string('country')->nullable();
--- resources/views/client/artists/index/artists.blade.php
+++ resources/views/client/artists/index/artists.blade.php
@@ -2,10 +2,12 @@
     <div class="h4 text-primary text-center py-3">
         <a href="{{ route('artists.index') }}" class="text-decoration-none link-primary">@lang('app.artists')</a>
     </div>
-    <div class="input-group px-5 my-3">
-        <input type="text" class="form-control" placeholder="@lang('app.searchArtist')">
-        <button class="btn btn-outline-secondary" type="button" id="button-addon2">@lang('app.search')</button>
-    </div>
+    <form class="d-flex input-group px-5 py-3" action="{{ url()->current() }}" method="get">
+        @csrf
+        <input type="text" class="form-control" name="q" value="{{ $f_q }}" placeholder="@lang('app.search')"
+               autocomplete="off">
+        <button class="btn btn-outline-secondary" type="submit">@lang('app.search')</button>
+    </form>
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
         @foreach($artists as $artist)
             @include('client.app.artist')
